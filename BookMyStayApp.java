14b1996cb35a4e45d85a56f2db5be5ddb1f127a3
Mukti Shree Jain <108231586+muktishreejain@users.noreply.github.com>
2026-03-17 03:11:11 +0000

    usecase8

---
+++ BookMyStayApp.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+class Reservation {
+
+    private String guestName;
+    private String roomType;
+
+    public Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+}
+
+class BookingHistory {
+
+    private List<Reservation> confirmedReservations;
+
+    public BookingHistory() {
+        confirmedReservations = new ArrayList<>();
+    }
+
+    public void addReservation(Reservation reservation) {
+        confirmedReservations.add(reservation);
+    }
+
+    public List<Reservation> getConfirmedReservations() {
+        return confirmedReservations;
+    }
+}
+class BookingReportService {
+
+    public void generateReport(BookingHistory history) {
+
+        System.out.println("Booking History Report");
+
+        for (Reservation r : history.getConfirmedReservations()) {
+            System.out.println(
+                    "Guest: " + r.getGuestName() +
+                    ", Room Type: " + r.getRoomType()
+            );
+        }
+    }
+}
+
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Booking History and Reporting\n");
+
+        BookingHistory history = new BookingHistory();
+
+        // Add confirmed bookings
+        history.addReservation(new Reservation("Abhi", "Single"));
+        history.addReservation(new Reservation("Subha", "Double"));
+        history.addReservation(new Reservation("Vanmathi", "Suite"));
+
+        BookingReportService reportService = new BookingReportService();
+
+        reportService.generateReport(history);
+    }
+}
