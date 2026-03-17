bf2d459cd9f5412b8a99a347777bd3589131fee7
Mukti Shree Jain <108231586+muktishreejain@users.noreply.github.com>
2026-03-17 03:05:22 +0000

    usecase7

---
+++ BookMyStayApp.java
@@ -0,0 +1,75 @@
+import java.util.*;
+class Service {
+
+    private String serviceName;
+    private double cost;
+
+    public Service(String serviceName, double cost) {
+        this.serviceName = serviceName;
+        this.cost = cost;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
+
+class AddOnServiceManager {
+
+    private Map<String, List<Service>> servicesByReservation;
+
+    public AddOnServiceManager() {
+        servicesByReservation = new HashMap<>();
+    }
+
+    public void addService(String reservationId, Service service) {
+
+        servicesByReservation
+                .computeIfAbsent(reservationId, k -> new ArrayList<>())
+                .add(service);
+    }
+
+    public double calculateTotalServiceCost(String reservationId) {
+
+        List<Service> services = servicesByReservation.get(reservationId);
+
+        if (services == null) {
+            return 0;
+        }
+
+        double total = 0;
+
+        for (Service s : services) {
+            total += s.getCost();
+        }
+
+        return total;
+    }
+}
+
+public class BookMyStayApp{
+
+    public static void main(String[] args) {
+
+        System.out.println("Add-On Service Selection");
+
+        String reservationId = "Single-1";
+
+        AddOnServiceManager manager = new AddOnServiceManager();
+
+        Service breakfast = new Service("Breakfast", 500);
+        Service spa = new Service("Spa", 1000);
+
+        manager.addService(reservationId, breakfast);
+        manager.addService(reservationId, spa);
+
+        double totalCost = manager.calculateTotalServiceCost(reservationId);
+
+        System.out.println("Reservation ID: " + reservationId);
+        System.out.println("Total Add-On Cost: " + totalCost);
+    }
+}
